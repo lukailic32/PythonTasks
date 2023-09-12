38641507befa34e1b08066538903b255f6bfd022
Luka Ilic <luka.ilic32@gmail.com>
2023-09-12 14:53:23 +0200

    Prepravljen treci zadatak

--- task3.py
+++ task3.py
@@ -1,11 +1,17 @@
-def task3():
-    uppers = 0
-    lowers = 0
-    sentence = input("Enter a sentence: ")
-    for ch in sentence:
-        if ch.isupper():
-            uppers = uppers + 1
-        elif ch.islower():
-            lowers = lowers + 1
+import sys
 
-    print(f"UPPER CASE {uppers} LOWER CASE {lowers}")
+if len(sys.argv) < 2:
+    print("Unesite recenicu kao argument")
+    exit()
+sentence = " ".join(sys.argv[1:])
+
+uppercases = 0
+lowercases = 0
+
+for ch in sentence:
+    if ch.isupper():
+        uppercases = uppercases + 1
+    elif ch.islower():
+        lowercases = lowercases + 1
+
+print(f"UPPER CASE {uppercases} LOWER CASE {lowercases}")
